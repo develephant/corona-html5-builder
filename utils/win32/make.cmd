b899090253a5bc36133b3da25d6c3f2ba17592ff
develephant <code@develephant.com>
2018-04-02 11:35:20 -0500

    build

--- utils/win32/make.cmd
+++ utils/win32/make.cmd
@@ -3,11 +3,8 @@
 rem Corona HTML5 Node Kit - Plugin Builder
 rem (c)2018 C. Byerley (develephant)
 
-set wd=%~dp0
 set buildlua=%1
 
 cd "%CORONA_PATH%"
 
 Native\Corona\win\bin\CoronaBuilder.exe build --lua %buildlua%
-
-cd %wd%
